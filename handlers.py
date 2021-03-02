b582170bc05d083d844858bba548107486d0cdb1
Rob Curtis <waab@me.com>
2021-03-02 18:54:56 -0500

    Handler and trigger updates

--- handlers.py
+++ handlers.py
@@ -5,15 +5,17 @@ Created on Mar 1, 2021
 '''
 
 import logging
-from util import trigger
+from util import triggers
 
 def handle_comment(comment):
     logging.debug('Handling comment [%s]', comment.id)
-    if trigger in comment.body.lower():
-        logging.info('Comment contains %s', trigger)
+    matches = {trigger for trigger in triggers.keys() if trigger in comment.body.lower()}
+    if len(matches) > 0:
+        logging.info('Comment [%s] contains %s : [%s]', comment.id, matches, comment.body)
         
     
 def handle_submission(submission):
     logging.debug('Handling submission [%s]', submission.id)
-    if submission.is_self and trigger in submission.selftext.lower():
-        logging.info('Submission contains %s', trigger)
+    matches = {trigger for trigger in triggers.keys() if trigger in submission.selftext.lower()}
+    if len(matches) > 0:
+        logging.info('Submission [%s] contains %s : [%s]', submission.id, matches, submission.selftext)
--- util.py
+++ util.py
@@ -7,7 +7,13 @@ import praw
 
 bot_name="DNBBot"
 user_agent="script:DNBBot:0.1 (by u/BourbonInExile)"
-trigger = 'paa'.lower()
+triggers = {
+    'paa': 'PAA',
+    'phoenix artisan accoutrements': 'PAA',
+    'phoenix shaving': 'PAA',
+    'ib ': 'IB',
+    'italian barber': 'IB'
+    }
 
 # Create the connection to Reddit.
 # This assumes a properly formatted praw.ini file exists:
@@ -15,4 +21,5 @@ trigger = 'paa'.lower()
 reddit = praw.Reddit(bot_name, user_agent=user_agent)
 
 # Get a handle on our preferred subreddit
-subreddit = reddit.subreddit("WetShaving+Wicked_Edge")
+# subreddit = reddit.subreddit("WetShaving+Wicked_Edge")
+subreddit = reddit.subreddit("WetShaving+Wicked_Edge+TrueWetShaving")
